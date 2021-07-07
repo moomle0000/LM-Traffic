7543c9c3495101c4eb2e2e023fee3e03bc434687
moomle00000 <68758566+moomle00000@users.noreply.github.com>
2021-07-08 01:49:52 +0300

    Update LM_Traffic.py

--- LM_Traffic.py
+++ LM_Traffic.py
@@ -1,55 +1,103 @@
- from selenium import webdriver
+import os
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.proxy import Proxy , ProxyType
 import time
+import requests
+import sys
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 import random
-import csv
+import re
+from fake_useragent import UserAgent
+from random import randint
 
-
-def login(PROXY,keyword,urls):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--proxy-server=%s' % PROXY)
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-            
-    with webdriver.Chrome(options=options) as driver:
-
-        print("[+] Google Search : "+keyword)
-        driver.get('https://www.google.com/search?q='+keyword)
-        time.sleep(6)
-        driver.get(urls)
-        print("[+] url : "+urls)
-        print("[+] keyword : "+keyword)
-        print("[+] Proxy : "+PROXY)
-        print()
-        time.sleep(10)
-        driver.quit
-
-count = 0
-count1 = 0
-count2 = 0
-while True:
-
-    lines1 = [line for line in open ("proxy.txt")]
-
-    count += 1
-    PROXY = lines1[count].strip()
-
-
-    lines2 = [line for line in open ("urls.txt")]
-
-    count1 += 1
-    urls = lines2[count].strip() 
-
-    lines3 = [line for line in open ("keyword.txt")]
-
-    count2 += 1
-    keyword = lines3[count].strip() 
-
+    
+def web():
     try:
+        ## Rotating Requests through a pool of Proxies ##
+        ua = UserAgent()
+        proxies = []
+        proxies_req = Request('https://www.sslproxies.org/')
+        proxies_req.add_header('User-Agent', ua.random)
+        proxies_doc = urlopen(proxies_req).read().decode('utf8')
+
+        soup = BeautifulSoup(proxies_doc, 'html.parser')
+        proxies_table = soup.find(id='proxylisttable')
+
+        # Save proxies in the array
+        for row in proxies_table.tbody.find_all('tr'):
+            proxies.append({
+            'ip':   row.find_all('td')[0].string,
+            'port': row.find_all('td')[1].string
+            })
+
+        # Choose a random proxy
+        proxy_index = random.randint(0, len(proxies) - 1)
+        proxy = proxies[proxy_index]
+
+
+        PROXY = proxy['ip'] + ':' + proxy['port']
         
-        login(PROXY,keyword,urls)
+        urls1 = ["https://lmlol.xyz","https://bot.lmlol.xyz","https://text.lmlol.xyz"]
+        urls = random.choice(urls1)
+        ##  urls  ##
+        print("[+] URL : "+urls)
+        print("[+] PROXY : "+PROXY)
         
-    except:
+
+        webdriver.DesiredCapabilities.CHROME['proxy'] = {
+        "httpProxy": PROXY,
+        "ftpProxy": PROXY,
+        "sslProxy": PROXY,
+        "proxyType":"MANUAL",
+        }
+
+        options = webdriver.ChromeOptions()
+        ##hide debuge webdrive##
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_argument('headless')
+        ua = UserAgent()
+        options.add_argument(f'user-agent={ua.random}')
+        options.add_argument("window-size=1400,600")
+        headers = {'referer':'https://google.com/'}
+        ##bot.lmlol.xyz##
+        if "https://bot.lmlol.xyz" in urls: 
+            with webdriver.Chrome(options=options , headers=headers) as driver:
+                driver.get(urls)
+                driver.implicitly_wait(5)
+                if driver.find_element_by_id('root'):
+                    print("[-] Finsh ")
+                    print()
+                driver.quit 
+        ##lmlol.xyz##
+        if "https://lmlol.xyz" in urls: 
+            with webdriver.Chrome(options=options) as driver:
+                driver.get(urls)
+                driver.implicitly_wait(5)
+                if driver.find_element_by_id('frmLogin'):
+                    print("[-] Finsh ")
+                    print()
+                driver.quit  
+        ##text.lmlol.xyz##
+        if "https://text.lmlol.xyz" in urls: 
+            with webdriver.Chrome(options=options) as driver:
+                driver.get(urls)
+                driver.implicitly_wait(5)
+                if driver.find_element_by_id('content'):
+                    print("[-] Finsh ")
+                    print()
+                driver.quit                                 
+    except Exception as e: 
         print()
         print("[+] Restart")
         print()
-        continue
+while True:                          
+    web()
+    
+
+
+
+
+
